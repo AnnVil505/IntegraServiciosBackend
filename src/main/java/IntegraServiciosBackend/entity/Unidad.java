e4fe165f50a075a19fa5914000c087b0d0fc6efc
AnnVil505 <danna13villanueva@gmail.com>
2025-11-06 17:05:17 -0500

    fix (Unidad): Eliminación de dependencias redundantes

--- src/main/java/IntegraServiciosBackend/entity/Unidad.java
+++ src/main/java/IntegraServiciosBackend/entity/Unidad.java
@@ -1,21 +1,11 @@
 package IntegraServiciosBackend.entity;
 
-import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import org.hibernate.annotations.Type;
-import java.util.Map;
 
 @Entity
 @Getter
